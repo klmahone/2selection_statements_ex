ef00a19ebf768dc8b7941d9011fbf010e4f1d824
klmahone <klmahone@gmail.com>
2020-08-25 15:28:33 -0400

    second commit

--- 2Selection_statements_ex/Program.cs
+++ 2Selection_statements_ex/Program.cs
@@ -6,7 +6,33 @@ namespace _2Selection_statements_ex
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("What is your favorite school subject?");
+
+            var subject = Console.ReadLine(); //evaluate to a string
+
+            switch (subject.ToLower())
+            {
+                case "Science":
+
+                    Console.WriteLine("Science is tough!");
+                    break;
+                case "math":
+                    Console.WriteLine("Math is tough!");
+                    break;
+                case "english":
+                    Console.WriteLine("English is cool!");
+                    break;
+                case "history":
+                    Console.WriteLine("History is interesting!");
+                    break;
+                case "pe":
+                    Console.WriteLine("PE is a lot fun!");
+                    break;
+                default:
+                    Console.WriteLine($"I never thought of that subject. {subject} sounds like a blast!");
+                    break;
+            }
         }
     }
 }
+ 
